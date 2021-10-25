2a4cfe1c82f7221c8fb9979f3656108bca7e75e7
fred <fl8750@laforest.biz>
2021-10-25 16:20:56 -0400

    Added un-flatten

--- FileTreeFlatten.ps1
+++ FileTreeFlatten.ps1
@@ -1,10 +1,25 @@
 ﻿
-Get-ChildItem -Path "X:\System\tmp\mpg\tmpg" -Recurse -file -include *.avi,*.divx,*.flv,*.m1v,*.m4v,*.mkv,*.mov,*.mp4,*.mpe,*.mpg,*.mpeg,*.rm,*.wmv | Select-Object -ExpandProperty FullName {
-    #$_
-    $Newdir = $_.FullName -replace 'X:\\System\\tmp\\mpg\\tmpg\\', '<root>\'
-    $Newdir2 = $Newdir -replace '\\','__'
-    $Newdir3 = $Newdir2 -replace '<root>', 'X:\System\tmp\mpg\tmpgFlat\_'
-    $Newdir3
-    #Out-File -FilePath $Newdir3
-    Move-Item -path $_.FullName -Destination $Newdir3
+$action = 'Rebuild'
+$oldRoot = "X:\System\tmp\mpg\tmpgFlat"
+$newRoot = "X:\System\tmp\mpg\tmpg"
+
+Get-ChildItem -Path $oldRoot -Recurse -file -include *.avi,*.divx,*.flv,*.m1v,*.m4v,*.mkv,*.mov,*.mp4,*.mpe,*.mpg,*.mpeg,*.rm,*.wmv | Select-Object -ExpandProperty FullName {
+    if ($action -ieq 'Flatten') {
+        $Newdir = $_.FullName -replace 'X:\\System\\tmp\\mpg\\tmpg\\', '<root>\'
+        $Newdir2 = $Newdir -replace '\\','__'
+        $Newdir3 = $Newdir2 -replace '<root>', $newRoot
+        $Newdir3
+        Move-Item -path $_.FullName -Destination $Newdir3
+    }
+
+    elseif ($action -ieq 'Rebuild') {
+        $Newdir = $newRoot + '\' + $_.Name
+        $Newdir2 = $Newdir -replace '___',''
+        $Newdir3 = $Newdir2 -replace '__error','_error'
+        $Newdir4 = $Newdir3 -replace '__','\'
+        $Newdir4
+        Move-Item -path $_.FullName -Destination $Newdir4
+    }
+
+
 }
